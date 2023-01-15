da827a8776d20f63106df9e361dbe5543c34581c
Juarez Santana <100575590+JuarezSSJ@users.noreply.github.com>
2023-01-15 12:02:42 -0300

    Inicio do projeto
    Projeto de cadastro de cliente, estudando CRUD

---
+++ banco.py
@@ -0,0 +1,10 @@
+# importando SQLite
+import sqlite3 as lite
+
+# Criando conexão
+conexao = lite.connect("dados.db")
+
+# Criando tabela
+with conexao:
+    cur = conexao.cursor()
+    cur.execute("CREATE TABLE formulario(id INTEGER PRIMARY KEY AUTOINCREMENT, nome TEXT, email TEXT, telefone TEXT, dia_em DATE, estado TEXT, assunto TEXT)")
---
+++ main.py
@@ -0,0 +1,35 @@
+# importando tkinter
+from tkinter import *
+
+################# cores ###############
+co0 = "#f0f3f5"  # Preta
+co1 = "#feffff"  # branca
+co2 = "#4fa882"  # verde
+co3 = "#38576b"  # valor
+co4 = "#403d3d"   # letra
+co5 = "#e06636"   # - profit
+co6 = "#038cfc"   # azul
+co7 = "#ef5350"   # vermelha
+co8 = "#263238"   # + verde
+co9 = "#e9edf5"   # sky blue
+
+# criando janela
+janela = Tk()
+janela.title("")
+janela.geometry('1043x453')
+janela.configure(background=co9)
+# bloquear tamanho da janela
+janela.resizable(width=False, height=False)
+
+# dividindo a janela por freme
+frame_superior = Frame(janela, width=310, height=50, bg=co2, relief='flat')
+frame_superior.grid(row=0, column=0)
+
+frame_superior = Frame(janela, width=310, height=403, bg=co1, relief='flat')
+frame_superior.grid(row=1, column=0, sticky=NSEW, padx=0, pady=1)
+
+frame_esq = Frame(janela, width=588, height=403, bg=co1, relief='flat')
+frame_esq.grid(row=0, column=1, rowspan=2, padx=1, pady=0, sticky=NSEW)
+
+
+janela.mainloop()
