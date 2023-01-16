129f77a2ea527dacfb765d83e1aa7e0ed3871b57
Juarez Santana <100575590+JuarezSSJ@users.noreply.github.com>
2023-01-16 14:51:57 -0300

    botão cadastrar
    criação do botão cadastrar

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # importando tkinter
 from tkinter import *
 from tkinter import ttk
-
+from tkinter import messagebox
 # importar calendario
 from tkcalendar import Calendar, DateEntry
+
+#importando VIEW
+from view import *
 
 
 ################# cores ###############
@@ -35,14 +38,46 @@ frame_superior.grid(row=0, column=0)
 frame_inferior = Frame(janela, width=310, height=403, bg=co1, relief='flat')
 frame_inferior.grid(row=1, column=0, sticky=NSEW, padx=0, pady=1)
 
-frame_esq = Frame(janela, width=588, height=403, bg=co1, relief='flat')
-frame_esq.grid(row=0, column=1, rowspan=2, padx=1, pady=0, sticky=NSEW)
+frame_dir = Frame(janela, width=588, height=403, bg=co1, relief='flat')
+frame_dir.grid(row=0, column=1, rowspan=2, padx=1, pady=0, sticky=NSEW)
 
 # criando o 1 label
 
 nome_app = Label(frame_superior, text="Formulário de Cadastro",
                   font=("Ivy 13 bold"), bg=co2, fg=co1, relief='flat', wraplength=310)
 nome_app.place(x=10, y=20)
+
+
+#função inserir / cadastrar
+
+def cadastrar():
+    nome = campo_nome.get()
+    email = campo_email.get()
+    telefone = campo_telefone.get()
+    data = campo_data_consulta.get()
+    situacao = campo_grau_urgencia.get()
+    obs = campo_obs.get()
+    
+    lista_cadastro = [nome, email, telefone, data, situacao, obs]
+
+    if nome == '':
+        messagebox.showerror('Erro', 'O nome não pode está vazio')
+    else:
+        cadastrar_info(lista_cadastro)
+        messagebox.showinfo('Sucesso', 'Os dados foram inseridos com sucesso')
+        #ira limpa os entry da posição 0 até o final de cada
+        campo_nome.delete(0, 'end')
+        campo_email.delete(0, 'end')
+        campo_telefone.delete(0, 'end')
+        campo_data_consulta.delete(0, 'end')
+        campo_grau_urgencia.delete(0, 'end')
+        campo_obs.delete(0, 'end')
+
+    for campos in frame_dir.winfo_children():
+        campos.destroy()
+    
+    apresentar()
+    
 
 # configurando frame_inferior
 
@@ -108,7 +143,7 @@ campo_obs.place(x=15, y=280)
 #cadastro dos botões
 
 #botão Cadastrar
-botao_cadastrar = Button (text="Cadastrar", borderwidth=2, relief="raised", overrelief="ridge", width=10, bg=co2, fg=co1)
+botao_cadastrar = Button (text="Cadastrar", command=cadastrar, borderwidth=2, relief="raised", overrelief="ridge", width=10, bg=co2, fg=co1)
 botao_cadastrar.place(x=20, y=370)
 
 #botão Editar
@@ -119,56 +154,58 @@ botao_cadastrar.place(x=110, y=370)
 botao_cadastrar = Button (text="Deletar", borderwidth=2, relief="raised", overrelief="ridge", width=10, bg=co7, fg=co1)
 botao_cadastrar.place(x=200, y=370)
 
+#construindo a função para apresentar a tabela
 
-# codigo para tabela
+def apresentar():
 
-lista = [[1,'Joao Futi Muanda','joao@mail.com', 123456789, "12/19/2010", 'Normal', 'consultar pessoalmente'],
-           [2,'Fortnato Mpngo', 'joao@mail.com', 123456789, "12/19/2010", 'Normal', 'consultar pessoalmente'],
-           [3,'Usando Python',  'joao@mail.com', 123456789, "12/19/2010", 'Normal', 'consultar pessoalmente'],
-           [4,'Clinton Berclidio', 'joao@mail.com', 123456789, "12/19/2010", 'Normal', 'consultar pessoalmente'],
-           [5,'A traicao da Julieta','joao@mail.com', 123456789, "12/19/2010", 'Normal', 'consultar pessoalmente']
-           ]
+    # codigo para tabela
+    lista = apresentar_info()
 
-# lista para cabecario
-tabela_head = ['ID','Nome',  'e-mail','Telefone', 'Data', 'Situação','Observação']
+    # lista para cabecario
+    tabela_head = ['ID','Nome',  'e-mail','Telefone', 'Data', 'Situação','Observação']
 
 
-# criando a tabela
-tree = ttk.Treeview(frame_esq, selectmode="extended", columns=tabela_head, show="headings")
+    # criando a tabela
+    tree = ttk.Treeview(frame_dir, selectmode="extended", columns=tabela_head, show="headings")
 
-# rolagem vertical - tem de ser colocar dentro do mesmo freme para funcionar ex.: frame_esq o mesmo que está a tabela tree
-rolagem_lateral = ttk.Scrollbar(frame_esq, orient="vertical", command=tree.yview)
+    # rolagem vertical - tem de ser colocar dentro do mesmo freme para funcionar ex.: frame_dir o mesmo que está a tabela tree
+    rolagem_lateral = ttk.Scrollbar(frame_dir, orient="vertical", command=tree.yview)
 
-# rolagem horizontal - tem de ser colocar dentro do mesmo freme para funcionar ex.: frame_esq o mesmo que está a tabela tree
-rolagem_inferior = ttk.Scrollbar( frame_esq, orient="horizontal", command=tree.xview)
+    # rolagem horizontal - tem de ser colocar dentro do mesmo freme para funcionar ex.: frame_dir o mesmo que está a tabela tree
+    rolagem_inferior = ttk.Scrollbar( frame_dir, orient="horizontal", command=tree.xview)
 
-#aplicação da rolagem
-tree.configure(yscrollcommand=rolagem_lateral.set, xscrollcommand=rolagem_inferior.set)
-
-
-tree.grid(column=0, row=0, sticky='nsew')
-rolagem_lateral.grid(column=1, row=0, sticky='ns')
-rolagem_inferior.grid(column=0, row=1, sticky='ew')
-
-frame_esq.grid_rowconfigure(0, weight=12)
+    #aplicação da rolagem
+    tree.configure(yscrollcommand=rolagem_lateral.set, xscrollcommand=rolagem_inferior.set)
 
 
-hd=["nw","nw","nw","center","center","center","center"]
-h=[30,170,140,100,90,60,120]
-n=0
+    tree.grid(column=0, row=0, sticky='nsew')
+    rolagem_lateral.grid(column=1, row=0, sticky='ns')
+    rolagem_inferior.grid(column=0, row=1, sticky='ew')
 
-#criei um laço de repetição para realizar toda a configuração da tabela
-for coluna in tabela_head:
-    tree.heading(coluna, text=coluna.title(), anchor=CENTER)
-    #ajusta a largura da coluna pelo tamanho do cabeçalho
-    tree.column(coluna, width=h[n],anchor=hd[n])
-    
-    n+=1
+    frame_dir.grid_rowconfigure(0, weight=12)
 
-for item in lista:
-    tree.insert('', 'end', values=item)
-    #end - sig que os valores serão inseridos no final de uma tabela
-    #values - é o que o for está percorrendo, e com o insert será inserido no final da lista
-    #insert serve para inserir os itens em uma tabela Treeview
+
+    hd=["nw","nw","nw","center","center","center","center"]
+    h=[30,170,140,100,90,60,120]
+    n=0
+
+    #criei um laço de repetição para realizar toda a configuração da tabela
+    for coluna in tabela_head:
+        tree.heading(coluna, text=coluna.title(), anchor=CENTER)
+        #ajusta a largura da coluna pelo tamanho do cabeçalho
+        tree.column(coluna, width=h[n],anchor=hd[n])
+        
+        n+=1
+
+    for item in lista:
+        tree.insert('', 'end', values=item)
+        #end - sig que os valores serão inseridos no final de uma tabela
+        #values - é o que o for está percorrendo, e com o insert será inserido no final da lista
+        #insert serve para inserir os itens em uma tabela Treeview
+
+#chamando a função apresentar
+apresentar()
+
+
 
 janela.mainloop()
--- view.py
+++ view.py
@@ -10,40 +10,48 @@ import sqlite3 as lite
 # criando a conexão com o bd
 conexao = lite.connect("dados.db")
 
-lista_cadastro = ['Juarez Santana',  'jssjr@gmail.com',
-                  71987757575, "01/15/2023", 'Normal', 'consultar pessoalmente']
-
 
 # função cadastrar
-with conexao:
-    cur = conexao.cursor()
-    # comandos do SQL
-    query = "INSERT INTO formulario (nome, email, telefone, data_consulta, situacao, observacao) VALUES (?, ?, ?, ?, ?, ?)"
-    # comando para executar o query
-    cur.execute(query, lista_cadastro)
+def cadastrar_info(i):
+    with conexao:
+        cur = conexao.cursor()
+        # comandos do SQL
+        query = "INSERT INTO formulario (nome, email, telefone, data_consulta, situacao, observacao) VALUES (?, ?, ?, ?, ?, ?)"
+        # comando para executar o query
+        cur.execute(query, i)
+
 
 
 # acessar informações
-with conexao:
-    cur = conexao.cursor()
-    # * - sig tudo
-    query = "SELECT * FROM formulario"
-    cur.execute(query)
-    info = cur.fetchall()
-    print(info)
+def apresentar_info():
+    lista_apresentar = []
+    with conexao:
+        cur = conexao.cursor()
+        # * - sig tudo
+        query = "SELECT * FROM formulario"
+        cur.execute(query)
+        info = cur.fetchall()
+        #corre toda a lista da info e mandar para a lista_apresentar
+        for i in info:
+            lista_apresentar.append(i)
+    return lista_apresentar
 
+"""
 
 lista_atualizar = ["Júnior", 1]
 # função atualizar
-with conexao:
-    cur = conexao.cursor()
-    query = "UPDATE formulario SET nome=? WHERE id=?"
-    cur.execute(query, lista_atualizar)
+def atualizar_info():
+    with conexao:
+        cur = conexao.cursor()
+        query = "UPDATE formulario SET nome=? WHERE id=?"
+        cur.execute(query, lista_atualizar)
 
 
 lista_deletar = [1]
 # função deletar
-with conexao:
-    cur = conexao.cursor()
-    query = "DELETE FROM formulario WHERE id=?"
-    cur.execute(query, lista_deletar)
+def deletar_info():
+    with conexao:
+        cur = conexao.cursor()
+        query = "DELETE FROM formulario WHERE id=?"
+        cur.execute(query, lista_deletar)
+"""
